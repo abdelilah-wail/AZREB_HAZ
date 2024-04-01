74587fc262a48fb960f72ae92b436ad369f68332
Nedjar Wail <nedjarwail@Nedjars-MacBook-Air.local>
2024-04-01 06:39:27 +0100

    i create the animations steps

---
+++ wail/main.js
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>AZREB HAZ</title>
+
+    <!--- CSS--->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- ICONS -->
+    <link rel="stylesheet"
+    href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+    <link
+    href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css"
+    rel="stylesheet"
+    />
+
+    <!-- GOOGLE FONTS  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Madimi+One&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <a href="#" class="logo">AZREB-HAZ!</a>
+        <ul class="navbar">
+            <li><a href="main.html">HOME</a></li>
+            <li><a href="game.html">GAME</a></li>
+            <li><a href="member.html">MEMBERS</a></li>
+            <li><a href="contact.html">CONTACT</a></li>
+        </ul>
+        <div class="bx bx-menu" id="menu-icon">
+
+        </div>
+    </header>
+
+    <section class="dobble">
+        <div class="dobbletext">
+            <h5>#1 Trend in Algeria</h5>
+            <h1>AZREB HAZ !!</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br>
+                Officiis nam possimus quo quisquam eveniet harum, tempora 
+                itaque quibusdam omnis sequi?</p>
+            <a href="#" class="ctaa"><i class="ri-play-fill"></i> Watch Game Play</a>    
+        </div>
+
+        <div class="Dimage">
+            <img src="azreb.jpg" alt="logo">
+        </div>
+    </section>
+
+    
+
+
+    <!-- SCRIPT -->
+    <script src="style.js"></script>
+</body>
+</html>
